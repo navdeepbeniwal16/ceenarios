25fe2f53324089c5c47208fba2d313d9ff8ed6b2
Navdeep Beniwal <navdeep.beniwal1997@gmail.com>
2024-02-14 15:49:12 +1100

    add scroll controller and store/read key from .env

--- lib/chatScreen.dart
+++ lib/chatScreen.dart
@@ -1,7 +1,13 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:talktune/context-manager.dart';
 import 'package:talktune/models/message.dart';
 import 'package:talktune/networking/openai-service.dart';
+// ignore: depend_on_referenced_packages
+import 'package:path_provider/path_provider.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -12,36 +18,81 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   List<Map<String, dynamic>> messages = [];
+  final TextEditingController _messageController = TextEditingController();
+  final ScrollController _scrollController = ScrollController();
 
   final openAIService = OpenAIService(
-      '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'); // TODO: Fetch it from environment variables
-  var contextManager = ContextManager(contextWindowSize: 25);
+      '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'); // TODO: Fetch it from environment variables
+  var contextManager = ContextManager(contextWindowSize: 100);
+
+  Future<String> readPersonalityProfile() async {
+    final String filePath =
+        'assets/agents/julie.txt'; // Path to your text file within your assets
+    try {
+      final String contents = await rootBundle.loadString(filePath);
+      return contents;
+    } catch (e) {
+      throw Exception('Failed to load file: $e');
+    }
+  }
 
   @override
   void initState() {
     super.initState();
+    asyncInitState();
+  }
 
-    // TODO: Fetch the messages context from the database
-    // initialising the state of the context-manager
-    contextManager.pushMessage("user",
-        "Simulate the personality of Shaye, and act as an agent for conversation. Shaye is a 23-year-old Australian university student majoring in English Literature. She aspires to be a writer and is working on her first novel. Shaye enjoys intellectual discussions, has a sense of humor, and prefers old-school interests over modern pop culture.");
-    contextManager.pushMessage("user",
-        "Hey, isn't that 'The Secret History' by Donna Tartt? I've been meaning to read that.");
-    contextManager.pushMessage(
-        "system", "Oh, yes, it is. It's quite an interesting read.");
-    contextManager.pushMessage("user",
-        "I'm Navdeep. I enjoy a good novel now and then. What are your thoughts on it?");
+  Future<void> asyncInitState() async {
+    try {
+      String personalitySetupPrompt = await readPersonalityProfile();
+      print('Agent Personality');
+      print(personalitySetupPrompt);
+      setState(() {
+        contextManager.pushMessage("user", personalitySetupPrompt);
+        messages = convertMessagesForDisplay();
+      });
+    } catch (e) {
+      print(e); // You may want to handle this error differently
+    }
+  }
+
+  List<Map<String, dynamic>> convertMessagesForDisplay() {
+    return contextManager.getMessages().map((message) {
+      return {
+        'isAgent': message.role == 'system',
+        'text': message.content,
+      };
+    }).toList();
   }
 
   Future<void> startChat() async {
-    List<Message> messages = contextManager.getMessages();
-    print('Messages formatted for request:');
-    print(messages);
+    // sendMessage(
+    //     "Ohh really? I feel like I should definetely give it a try. You sound very convincing. I'm Navdeep by the way.");
+  }
 
-    List<Map<String, dynamic>> messagesJson = contextManager.messagesToJson();
-    String response = await openAIService.getChatResponse(messagesJson);
+  Future<void> sendMessage(String message) async {
+    setState(() {
+      contextManager.pushMessage('user', message);
+      messages = convertMessagesForDisplay();
+    });
+    List<Map<String, dynamic>> messagesFromContext =
+        contextManager.messagesToJson();
+
+    String response = await openAIService.getChatResponse(messagesFromContext);
     print('Printing response from OpenAI chat API');
     print(response);
+
+    setState(() {
+      contextManager.pushMessage('system', response);
+      messages = convertMessagesForDisplay();
+      Future.delayed(Duration(milliseconds: 100), () {
+        _scrollController.animateTo(
+          _scrollController.position.maxScrollExtent,
+          duration: Duration(milliseconds: 300),
+          curve: Curves.easeOut,
+        );
+      });
+    });
   }
 
   void stopChat() {
@@ -68,55 +119,38 @@ class _ChatScreenState extends State<ChatScreen> {
           },
         ),
       ),
-      body: ChatBody(messages: messages),
-      bottomNavigationBar: Container(
-        height: 150, // Increased height
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.bottomCenter,
-            end: Alignment.topCenter,
-            colors: [
-              bottomBarColor, // Color of the bottom bar
-              chatBodyColor, // Color of the chat body
-            ],
+      body: Column(
+        children: [
+          Expanded(
+              child:
+                  ChatBody(controller: _scrollController, messages: messages)),
+          Padding(
+            padding: const EdgeInsets.all(35.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _messageController,
+                    decoration: InputDecoration(
+                      hintText: 'Type a message',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.send),
+                  iconSize: 35.0,
+                  onPressed: () {
+                    sendMessage(_messageController.text);
+                    _messageController.clear();
+                  },
+                ),
+              ],
+            ),
           ),
-        ),
-        child: BottomAppBar(
-          color: Colors.transparent, // Make BottomAppBar background transparent
-          elevation: 0, // Remove shadow if not needed
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Icon(
-                  isAgentSpeaking
-                      ? Icons.headset_mic_outlined
-                      : Icons.headset_mic,
-                  color: isAgentSpeaking
-                      ? Theme.of(context).colorScheme.primary
-                      : Colors.black), // Icon indicating the agent
-              FloatingActionButton(
-                child: Text(!isChatActive ? "Start" : "Stop"), // Start button
-                onPressed: () {
-                  // Stop button action
-                  setState(() {
-                    isChatActive = !isChatActive;
-                    if (isChatActive) {
-                      startChat();
-                    } else {
-                      stopChat();
-                    }
-                  });
-                },
-              ),
-
-              Icon(!isAgentSpeaking ? Icons.mic_none_outlined : Icons.mic_none,
-                  color: !isAgentSpeaking
-                      ? Theme.of(context).colorScheme.primary
-                      : Colors.black), // Icon indicating the user
-            ],
-          ),
-        ),
+        ],
       ),
     );
   }
@@ -124,7 +158,8 @@ class _ChatScreenState extends State<ChatScreen> {
 
 class ChatBody extends StatefulWidget {
   final List<Map<String, dynamic>> messages;
-  const ChatBody({super.key, required this.messages});
+  final ScrollController controller;
+  const ChatBody({super.key, required this.messages, required this.controller});
 
   @override
   _ChatBodyState createState() => _ChatBodyState();
@@ -134,6 +169,7 @@ class _ChatBodyState extends State<ChatBody> {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
+      controller: widget.controller,
       itemCount: widget.messages.length,
       itemBuilder: (context, index) {
         bool isAgent = widget.messages[index]['isAgent'];
