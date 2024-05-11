1559e1ccc63073bf2a32c72f6c7ec87d84f62d8a
Navdeep Beniwal <navdeep.beniwal1997@gmail.com>
2024-05-11 17:16:17 +1000

    add api to get transcription of audio file in jobs

--- server/routes/jobs.js
+++ server/routes/jobs.js
@@ -1,15 +1,24 @@
 // Load environment variables
 require("dotenv").config();
-const OpenAI = require("openai");
 
-// Initialise OpenAI services
+// Import dependencies
+const fs = require("fs");
+const OpenAI = require("openai");
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const { Readable } = require("stream");
+
+// Initialise services
 const openai = new OpenAI({
   apiKey: process.env.OPEN_AI_ACCESS_KEY,
   dangerouslyAllowBrowser: true,
 });
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
-const express = require("express");
-const router = express.Router();
+// Constants
+const TEMPORARY_AUDIO_FILENAME = "temp_audio_file.mp3";
 
 const getQuestionsSet = async (company, role, description) => {
   const completion = await openai.chat.completions.create({
@@ -48,11 +57,46 @@ const getQuestionsSet = async (company, role, description) => {
   return questions;
 };
 
+// function to save buffer to a temporary file
+const bufferToFile = async (buffer, filename) => {
+  fs.writeFileSync(filename, buffer);
+  return filename;
+};
+
+// function to transcribe an audio file using openai transcription api
+const transcribeAudio = async (audioFile) => {
+  try {
+    console.log("Attempting transcription of audioFile.");
+    const buffer = audioFile.buffer;
+    const audioFilename = await bufferToFile(buffer, TEMPORARY_AUDIO_FILENAME);
+    console.log(`Saved file temporarily as ${audioFilename}.`);
+    const fileStream = fs.createReadStream(audioFilename);
+    const transcription = await openai.audio.transcriptions.create({
+      file: fileStream,
+      model: "whisper-1",
+      response_format: "verbose_json",
+      timestamp_granularities: ["word"],
+    });
+
+    console.log(
+      "Transcription request is successful. Transcribed text:",
+      transcription.text
+    );
+
+    fs.unlinkSync(audioFilename);
+    console.log(`Temporary file ${audioFilename} in unlinked.`);
+
+    return transcription;
+  } catch (error) {
+    console.error("Error transcribing audio:", error);
+  }
+};
+
 router.get("/", (req, res, next) => {
   return res.json({ message: "jobs api is called..." });
 });
 
-router.post("/", async (req, res) => {
+router.post("/fetch-behavioural-questions", async (req, res) => {
   const { company, role, description } = req.body;
 
   if (!company || !role) {
@@ -80,5 +124,47 @@ router.post("/", async (req, res) => {
     });
   }
 });
+
+router.post(
+  "/fetch-audio-transcription",
+  upload.single("file"),
+  async (req, res, next) => {
+    if (!req.file) {
+      console.log("No file uploaded.");
+      return res.status(400).send("No file uploaded.");
+    }
+
+    console.log(
+      `Received audio file with ${req.file.size} bytes for transcription`
+    );
+
+    try {
+      console.log(
+        "Making transcription request to the transcribeAudio function."
+      );
+      const resultTranscription = await transcribeAudio(req.file);
+
+      if (!resultTranscription || !resultTranscription.text) {
+        res.status(500).send("Failed to transcribe audio.");
+      }
+
+      const resultTranscriptionText = resultTranscription.text;
+      console.log("Transcription request is successful");
+
+      res.status(200).json({
+        message: "Audio file transcribed successfully! ",
+        transcription: {
+          text: resultTranscriptionText,
+        },
+      });
+    } catch (error) {
+      console.error("Error transcribing audio:", error);
+      res.status(500).send({
+        message: "Failed to transcribe audio.",
+        errorDescription: error,
+      });
+    }
+  }
+);
 
 module.exports = router;
