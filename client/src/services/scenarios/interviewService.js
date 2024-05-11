22da75e5b1759e31d3c4f2578db7eb2f28a93291
Navdeep Beniwal <navdeep.beniwal1997@gmail.com>
2024-05-11 17:17:03 +1000

    add function to InterviewService to fetch audio transcription for jobs

--- client/src/services/scenarios/interviewService.js
+++ client/src/services/scenarios/interviewService.js
@@ -3,11 +3,14 @@ import backendApiClient from "../core/backendAPIClient";
 class InterviewService {
   static async fetchBehaviouralQuestions(company, role, description) {
     try {
-      const response = await backendApiClient.post("/scenarios/jobs", {
-        company: company,
-        role: role,
-        description: description,
-      });
+      const response = await backendApiClient.post(
+        "/scenarios/jobs/fetch-behavioural-questions",
+        {
+          company: company,
+          role: role,
+          description: description,
+        }
+      );
 
       console.log("Response:", response);
 
@@ -29,6 +32,39 @@ class InterviewService {
     } catch (error) {
       console.error(`InterviewService: Error fetching questions: ${error}`);
       throw error;
+    }
+  }
+
+  static async fetchAudioTranscription(blob) {
+    const formData = new FormData();
+    formData.append("file", blob, "audio-file.ogg");
+
+    try {
+      const response = await fetch(
+        "/scenarios/jobs/fetch-audio-transcription",
+        {
+          method: "POST",
+          headers: {},
+          body: formData,
+        }
+      );
+
+      if (response.status !== 200) {
+        throw new Error("Request unsuccessful: " + response);
+      }
+
+      const data = await response.json();
+      if (!data.transcription) {
+        throw new Error(
+          "Transcripts not found in response payload: " + response
+        );
+      }
+
+      const transcriptionObj = data.transcription;
+      return transcriptionObj;
+    } catch (error) {
+      console.error("Failed to upload audio:", error);
+      return null;
     }
   }
 }
