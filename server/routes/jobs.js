5ad41720c9004771f432182904ff14a0e9c70785
Navdeep Beniwal <navdeep.beniwal1997@gmail.com>
2024-05-12 18:14:12 +1000

    moved routes logic to middlewares & add api to evaluate audio response

--- server/routes/jobs.js
+++ server/routes/jobs.js
@@ -125,45 +125,172 @@ router.post("/fetch-behavioural-questions", async (req, res) => {
   }
 });
 
+const fetchAudioTransacription = async (req, res, next) => {
+  if (!req.file) {
+    console.log("No file uploaded.");
+    return res.status(400).send("No file uploaded.");
+  }
+
+  console.log(
+    `Received audio file with ${req.file.size} bytes for transcription`
+  );
+
+  try {
+    console.log(
+      "Making transcription request to the transcribeAudio function."
+    );
+    const resultTranscription = await transcribeAudio(req.file);
+
+    if (!resultTranscription || !resultTranscription.text) {
+      res.status(500).send("Failed to transcribe audio.");
+    }
+
+    const resultTranscriptionText = resultTranscription.text;
+    console.log("Transcription request is successful");
+
+    const transcription = {
+      text: resultTranscriptionText,
+    };
+    req.transcription = transcription;
+    next();
+  } catch (error) {
+    console.error("Error transcribing audio:", error);
+    res.status(500).send({
+      message: "Failed to transcribe audio.",
+      errorDescription: error,
+    });
+  }
+};
+
 router.post(
   "/fetch-audio-transcription",
   upload.single("file"),
-  async (req, res, next) => {
-    if (!req.file) {
-      console.log("No file uploaded.");
-      return res.status(400).send("No file uploaded.");
+  fetchAudioTransacription,
+  (req, res, next) => {
+    const transcription = req.transcription;
+    res.status(200).json({
+      message: "Audio file transcribed successfully!",
+      transcription: transcription,
+    });
+  }
+);
+
+const evaluateResponse = async (req, res, next) => {
+  const requestBody = req.body;
+  try {
+    if (!requestBody) {
+      res.status(400).json({
+        message: "Error evaluating response",
+        description: "Request body is missing",
+      });
     }
 
-    console.log(
-      `Received audio file with ${req.file.size} bytes for transcription`
-    );
+    if (!requestBody.questionText) {
+      res.status(400).json({
+        message: "Error evaluating response",
+        description: "Request body is missing 'questionText'",
+      });
+    }
 
-    try {
-      console.log(
-        "Making transcription request to the transcribeAudio function."
-      );
-      const resultTranscription = await transcribeAudio(req.file);
+    if (!requestBody.responseText) {
+      res.status(400).json({
+        message: "Error evaluating response",
+        description: "Request body is missing 'responseText'",
+      });
+    }
 
-      if (!resultTranscription || !resultTranscription.text) {
-        res.status(500).send("Failed to transcribe audio.");
-      }
+    if (!requestBody.companyName) {
+      res.status(400).json({
+        message: "Error evaluating response",
+        description: "Request body is missing 'companyName'",
+      });
+    }
 
-      const resultTranscriptionText = resultTranscription.text;
-      console.log("Transcription request is successful");
+    if (!requestBody.jobRole) {
+      res.status(400).json({
+        message: "Error evaluating response",
+        description: "Request body is missing 'jobRole'",
+      });
+    }
 
-      res.status(200).json({
-        message: "Audio file transcribed successfully! ",
-        transcription: {
-          text: resultTranscriptionText,
+    const { questionText, responseText, companyName, jobRole, jobDescription } =
+      requestBody;
+
+    const completion = await openai.chat.completions.create({
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are trained to evaluate behavioral interview questions response based on specific job details.",
         },
-      });
-    } catch (error) {
-      console.error("Error transcribing audio:", error);
-      res.status(500).send({
-        message: "Failed to transcribe audio.",
-        errorDescription: error,
-      });
+        {
+          role: "user",
+          content: `Evaluate the following response to the interview question for a job role at ${companyName}. The job title is ${jobRole}. \n Here's is the job description (Could be null as well): ${jobDescription}. \n Here's the questions and response pair: \n Question: ${questionText} \n ${responseText}`,
+        },
+        {
+          role: "user",
+          content:
+            "Based on the information provided, evaluate the reponse on the following aspects: \n Relevance: How well the response adddress the question \n Delivery: Clarity and fluency of the response \n Tone: Appropriateness of the tone given the formal setting \n Please also include snippets from response in your feedback to be more specific" +
+            "Please provide the results in the following json format: \n" +
+            "{results: " +
+            "{ relevance: {strengths: [dot points highlighting the strengths], weaknesses: [dot points highlighting the weaknesses], waysToImprove: [dot points highlighting ways to improve], score: A score out of 10}" +
+            "{ delivery: {strengths: [dot points highlighting the strengths], weaknesses: [dot points highlighting the weaknesses], waysToImprove: [dot points highlighting ways to improve], score: A score out of 10}" +
+            "{ tone: {strengths: [dot points highlighting the strengths], weaknesses: [dot points highlighting the weaknesses], waysToImprove: [dot points highlighting ways to improve], score: A score out of 10}" +
+            "}}",
+        },
+      ],
+      model: "gpt-3.5-turbo",
+      response_format: { type: "json_object" },
+    });
+
+    // Parse raw json response and extract evaluation results
+    // Throw error is response is not of array type or is empty
+    if (!Array.isArray(completion.choices) || completion.choices.length < 0) {
+      throw new Error(
+        "Error occured in OpenAI chat api response while generating evaluating response for the question and role details."
+      );
     }
+
+    const evaluationResultsRawJSON = completion.choices[0].message.content;
+    const evaluationResults = Object(JSON.parse(evaluationResultsRawJSON))[
+      "results"
+    ];
+
+    req.results = evaluationResults;
+    next();
+  } catch (error) {
+    console.log("Error evaluating response:", error);
+    res.status(500).json({
+      message: "Unknown error evaluating response",
+      description: error,
+    });
+  }
+};
+
+router.post("/evaluate-response-text", evaluateResponse, (req, res, next) => {
+  const results = req.results;
+  res.json({
+    message: "Question response successfully evaluated",
+    results: results,
+  });
+});
+
+router.post(
+  "/evaluate-response-audio",
+  upload.single("file"),
+  fetchAudioTransacription,
+  (req, res, next) => {
+    const responseTranscription = req.transcription;
+    req.body.responseText = responseTranscription.text;
+    next();
+  },
+  evaluateResponse,
+  (req, res, next) => {
+    const results = req.results;
+    res.json({
+      message: "Question response successfully evaluated",
+      results: results,
+    });
   }
 );
 
